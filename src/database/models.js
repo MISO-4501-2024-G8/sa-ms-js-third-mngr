0fa4134d73d5b446850a50b6c651994fb26c4032
Juan Sebastian Sanchez L. <js.sanchezl12@uniandes.edu.co>
2024-04-13 17:17:08 -0500

    Update dependencies and add CORS support

--- src/database/models.js
+++ src/database/models.js
@@ -7,134 +7,149 @@ class Models {
         this.sequelize = sequelize;
     }
 
-    defineUser(){
-        return this.sequelize.define('user', {
-            id: {
-                type: DataTypes.STRING,
-                primaryKey: true,
-                autoIncrement: true
+    defineUser() {
+        return this.sequelize.define('user',
+            {
+                id: {
+                    type: DataTypes.STRING,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                email: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                password: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                doc_num: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                doc_type: {
+                    type: DataTypes.STRING(10),
+                    allowNull: false
+                },
+                name: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                phone: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                user_type: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                token: {
+                    type: DataTypes.STRING,
+                    allowNull: true
+                },
+                expiration_token: {
+                    type: DataTypes.DATE,
+                    allowNull: true
+                }
             },
-            email: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            password: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            doc_num: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            doc_type: {
-                type: DataTypes.STRING(10),
-                allowNull: false
-            },
-            name: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            phone: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            user_type: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            token: {
-                type: DataTypes.STRING,
-                allowNull: true
-            },
-            expiration_token: {
-                type: DataTypes.DATE,
-                allowNull: true
+            {
+                tableName: 'user'
             }
-        });
+        );
     }
 
     defineSportUser() {
-        return this.sequelize.define('sport_user', {
-            id: {
-                type: DataTypes.STRING,
-                primaryKey: true,
-                autoIncrement: true
+        return this.sequelize.define('sport_user',
+            {
+                id: {
+                    type: DataTypes.STRING,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                gender: {
+                    type: DataTypes.STRING(5),
+                    allowNull: false
+                },
+                age: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                weight: {
+                    type: DataTypes.FLOAT,
+                    allowNull: false
+                },
+                height: {
+                    type: DataTypes.FLOAT,
+                    allowNull: false
+                },
+                birth_country: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                birth_city: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                residence_country: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                residence_city: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                residence_seniority: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                sports: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                acceptance_notify: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                acceptance_tyc: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                acceptance_personal_data: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                }
             },
-            gender: {
-                type: DataTypes.STRING(5),
-                allowNull: false
-            },
-            age: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            weight: {
-                type: DataTypes.FLOAT,
-                allowNull: false
-            },
-            height: {
-                type: DataTypes.FLOAT,
-                allowNull: false
-            },
-            birth_country: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            birth_city: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            residence_country: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            residence_city: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            residence_seniority: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            sports: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            acceptance_notify: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            acceptance_tyc: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            acceptance_personal_data: {
-                type: DataTypes.INTEGER,
-                allowNull: false
+            {
+                tableName: 'sport_user'
             }
-        });
+        );
     }
 
     defineThirdUser() {
-        return this.sequelize.define('third_user', {
-            id: {
-                type: DataTypes.STRING,
-                primaryKey: true,
-                autoIncrement: true
+        return this.sequelize.define('third_user',
+            {
+                id: {
+                    type: DataTypes.STRING,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                company_creation_date: {
+                    type: DataTypes.DATE,
+                    allowNull: false
+                },
+                company_address: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                contact_name: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                }
             },
-            company_creation_date: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            company_address: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            contact_name: {
-                type: DataTypes.STRING,
-                allowNull: false
+            {
+                tableName: 'third_user'
             }
-        });
+        );
     }
 }
 
